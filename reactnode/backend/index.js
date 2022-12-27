767988c3cc24c32d7f9c24844c58841b8f0f9a63
Jawwad <jawwad.nissar@gmail.com>
2022-12-28 01:02:18 +0500

    signature in progress

--- reactnode/backend/index.js
+++ reactnode/backend/index.js
@@ -4,6 +4,10 @@ const mysql = require("mysql");
 const cors = require("cors");
 const { Configuration, OpenAIApi } = require("openai");
 require("dotenv").config();
+var Eth = require('web3-eth');
+
+// "Eth.providers.givenProvider" will be set if in an Ethereum supported browser.
+var eth = new Eth(Eth.givenProvider);
 
 const configuration = new Configuration({
   apiKey: process.env.Open_AI_Key,
@@ -26,6 +30,24 @@ app.post("/create", (req, res) => {
     //console.log(response.data.data[0].url);
     res.send(response.data.data[0].url);
   })();
+});
+
+app.get("/testsign", (req, res) => {
+  const privateKey = '0x70693de94d1c5efb66e055c379f022bcb2d4b0585223dbd8c5c13cc49e7c3e69';
+  const account = eth.accounts.privateKeyToAccount(privateKey);
+  eth.accounts.wallet.add(account);
+  eth.defaultAccount = account.address;
+  debugger;
+  eth.sign("jawad",eth.defaultAccount).then(obj => {
+    console.log(obj);
+})
+
+});
+
+app.get("/verify", (req, res) => {
+  debugger;
+  eth.personal.ecRecover("jawad", "0x5d4e2667ce41e0804924c0defc48d9c732348eb7e9bb0c0e93f90ec7da4ca2337d93d45c3446c4b1a1da9e3f6526d4768b1f37dd09647af8576cdaad847e98b41b").then(console.log);
+
 });
 
 app.listen(3001, () => {
